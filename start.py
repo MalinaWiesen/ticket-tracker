dedd4974f94bcad22db579fbb75a96c38c1a44f2
Malina Wiesen <MalinaWiesen@Malinas-MBP.hsd1.ca.comcast.net>
2015-12-15 08:19:35 -0800

    First Commit

---
+++ start.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import requests
+from pprint import pprint
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def hello():
+    return render_template('hello.html')
+
+@app.route("/loggedin")
+def welcome():
+
+    code = request.args.get('code', '')
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+
+    post_data = "code=%s&client_secret=%s&client_id=%s&grant_type=authorization_code" % (
+        code,
+        'EKGPOCDDNCLLUCXYL3T6Y36GLKS6AF3WQ7BMRKPPHLYHM2ZRQT',
+        'BOIHE6CFAPR5PW2H5Y')
+
+
+    response = requests.post('https://www.eventbrite.com/oauth/token',
+                            data=post_data,
+                             headers=headers).json()
+
+    access_token = response['access_token']
+
+    response = requests.get(
+    "https://www.eventbriteapi.com/v3/users/me/orders/",
+    headers = {
+        "Authorization": "Bearer " + access_token,
+    },
+    verify = True,  # Verify SSL certificate
+    ).json()
+
+    pprint(response)
+
+    total_cost = 0
+    for event in response['orders']:
+        event_cost = event['costs']['gross']['value']
+        total_cost += float(event_cost)
+
+    total_cost /= 100
+
+    return render_template('loggedin.html', total_cost="%.2f" % total_cost)
+
+if __name__ == "__main__":
+    app.run()
