50c2b0763e7eec73d4ac7ba264e305c2e3dc7311
Malina Wiesen <MalinaWiesen@Malinas-MBP.hsd1.ca.comcast.net>
2015-12-15 15:11:38 -0800

    adds bells and whistles

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 import requests
 from pprint import pprint
+import random
 
 app = Flask(__name__)
 app.debug = True
@@ -37,16 +38,33 @@ def welcome():
     verify = True,  # Verify SSL certificate
     ).json()
 
-    pprint(response)
 
+    upcoming_events = requests.get(
+    "https://www.eventbriteapi.com/v3/events/search/?start_date.range_start=2016-01-01T00:00:00Z&start_date.range_end=2016-01-30T00:00:00Z",
+    headers = {
+        "Authorization": "Bearer " + access_token,
+    },
+    verify = True,  # Verify SSL certificate
+    ).json()
+
+
+    random_index = random.randint(0, 49)
+    random_event_uri = upcoming_events['events'][random_index]['resource_uri']
+    random_event_name = upcoming_events['events'][random_index]['name']['html']
+    print (random_event_uri)
+    print (random_event_name)
+
+    events = 0
     total_cost = 0
     for event in response['orders']:
-        event_cost = event['costs']['gross']['value']
-        total_cost += float(event_cost)
+        if (event['created']).startswith('2015'):
+            events += 1
+            event_cost = event['costs']['gross']['value']
+            total_cost += float(event_cost)
 
     total_cost /= 100
 
-    return render_template('loggedin.html', total_cost="%.2f" % total_cost)
+    return render_template('loggedin.html', total_cost="%.2f" % total_cost, events=events, random_event_uri=random_event_uri, random_event_name=random_event_name)
 
 if __name__ == "__main__":
     app.run()
